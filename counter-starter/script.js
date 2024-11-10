c629f0b13724350c0090621d6187c4280d6a50ee
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-11-10 12:26:34 +1000

    reset

---
+++ counter-starter/script.js
@@ -0,0 +1,29 @@
+const add = (step) => {
+    console.log("add function");
+// We gave the number element an id of number so we can use that to get the element
+    let numberElement = document.getElementById("number");
+// We can use the innerText property to get the text inside the element
+    let number = numberElement.innerText;
+    console.log("add function", number);
+    number = parseInt(number) + step;
+    console.log("add function complete", number);
+        numberElement.innerText = number;
+};
+
+const minus = (step) => {
+    console.log("minus function");
+// We gave the number element an id of number so we can use that to get the element
+    let numberElement = document.getElementById("number");
+// We can use the innerText property to get the text inside the element
+    let number = numberElement.innerText;
+    console.log("minus function", number);
+    number = parseInt(number) - step;
+    console.log("add function complete", number);
+        numberElement.innerText = number;
+};
+
+function reset() {
+    let number = 0;
+    document.getElementById("number").innerHTML = number;
+    console.log("reset" + " " + (parseInt(0)));
+}
