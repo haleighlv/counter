8fe9060c48245bc2dfaa18ded262c0f898b6e31a
Haleigh Vinicombe <haleighvinicombe@gmail.com>
2024-11-12 09:24:29 +1000

    add in  1st JS

--- todo-list-starter/script.js
+++ todo-list-starter/script.js
@@ -1,3 +1,15 @@
+let todoTasks = ["Walk Chilli", "Make Dinner"];
+let todoTasksStatus = [false, true];
+updateTodoList();
+
+const todoList = document.getElementById("todo-list");
+
+for (const [index, task] of todoTasks.entries()) {
+    const newTodoTaskElement = createNewTodoItemElement(task, index);
+// Add the <li> element to the list
+todoList.appendChild(newTodoTaskElement);
+}
+
 const addTask = () => {
 
 };
@@ -7,9 +19,43 @@ const updateTodoList = () => {
 };
 
 const createNewTodoItemElement = (task, index) => {
+// Create a <p> element to store the task description
+const newTodoTaskTextElement = document.createElement("p");
+newTodoTaskTextElement.innerText = task;
+
+// Apply a CSS class to the completed items
+if (todoTasksStatus[index] == true) {
+newTodoTaskTextElement.classList.add("complete");
+}  
+
+// Create a <li> element to contain the paragraph
+const newTodoTaskElement = document.createElement("li");
+newTodoTaskElement.appendChild(newTodoTaskTextElement);
+
+// Adding a button to mark each item as complete
+const completeButtonElement = document.createElement("input");
+completeButtonElement.type = "button";
+completeButtonElement.value = "Completed";
+completeButtonElement.onclick = function () {
+toggleComplete(index);
+};
+newTodoTaskElement.appendChild(completeButtonElement);
+return newTodoTaskElement;
 
 };
 
 const toggleComplete = (index) => {
+// If it is complete, set it to incomplete. 
+// If it is incomplete, set it to complete.
+if (todoTasksStatus[index] == false) {
+todoTasksStatus[index] = true;
+} else {
+    todoTasksStatus[index] = false;
+}
+    updateTodoList();
+
     
 };
+
+
+
